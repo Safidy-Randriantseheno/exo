68e501b1824580fb645a4e9101ad41f0ffea6319
Randriantseheno Manitra Safidy Michael <hei.safidy.7@gmail.com>
2023-01-05 05:24:39 +0300

    update

--- src/main/resources/db/migration/V0_4__create_table_play_against.sql
+++ src/main/resources/db/migration/V0_4__create_table_play_against.sql
@@ -2,5 +2,7 @@ create table play_against
 (
     id_play_against serial primary key,
     datetime date,
-    stadium varchar
+    stadium varchar,
+    id_team_one int references team(id_team),
+    id_team_two int references team(id_team),
 );
--- src/main/resources/db/migration/V0_9_insert_into_play_against.sql
+++ src/main/resources/db/migration/V0_9_insert_into_play_against.sql
@@ -1,8 +1,8 @@
-insert into play_against (id_play_against, datetime, stadium)
-values (4, '2003-9-3', 'BAREA');
-insert into play_against (id_play_against, datetime, stadium)
-values (1, '2003-3-3', 'BAREA');
-insert into play_against (id_play_against, datetime, stadium)
-values (7, '2003-8-3', 'BAREA')
+insert into play_against (id_play_against, datetime, stadium, id_team_one, id_team_two)
+values (4, '2003-9-3', 'BAREA', 1, 3);
+insert into play_against (id_play_against, datetime, stadium, id_team_one, id_team_two)
+values (1, '2003-3-3', 'BAREA', 3, 2);
+insert into play_against (id_play_against, datetime, stadium, id_team_one, id_team_two)
+values (7, '2003-8-3', 'BAREA', 1, 2)
 ;
 alter sequence play_against_id_play_against_seq restart with 4;
